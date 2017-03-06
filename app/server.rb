561bdf96532de6255cf9468ee7330d2d0457ba16
Matthew Perry <thesedatedprince@gmail.com>
2017-03-06 11:16:50 +0000

    Server can now serve up get routes

--- app/server.rb
+++ app/server.rb
@@ -1,16 +1,49 @@
 require 'socket'
+require 'uri'
+
+WEB_ROOT = 'app/views'
+
+CONTENT_MAPPING = {
+    'html' => 'text/html',
+    'txt' => 'text/plain'
+}
+
+DEFAULT_CONTENT_TYPE = 'application/octet-stream'
+
+def content_type(path)
+    ext = File.extname(path).split(".").last
+    CONTENT_MAPPING.fetch(ext, DEFAULT_CONTENT_TYPE)
+end
+
+def requested_file(request_line)
+    request_uri = request_line.split(" ")[1]
+    path = URI.unescape(URI(request_uri).path)
+    File.join(WEB_ROOT, path)
+end
 
 server = TCPServer.new('localhost',4000)
 
 loop{
     socket = server.accept
-    request = socket.gets
-    STDERR.puts request
-    response = "Hello World!\n"
-    socket.print "HTTP/1.1 200 OK\r\n" + "Content-Type: text/plain\r\n" +
-    "Content-Length: #{response.bytesize}\r\n" +
-    "Connection: close\r\n"
-    socket.print "\r\n"
-    socket.print response
+    request_line = socket.gets
+
+    STDERR.puts request_line
+
+    path = requested_file(request_line)
+    if File.exist?(path) && !File.directory?(path)
+        File.open(path, "rb") do |file|
+            socket.print "HTTP/1.1 200 OK\r\n" + "Content-Type: #{content_type(file)}\r\n" +
+            "Content-Length: #{file.size}\r\n" +
+            "Connection: close\r\n"
+            socket.print "\r\n"
+            IO.copy_stream(file, socket)
+        end
+    else
+        message = "File not found\n"
+        socket.print "HTTP/1.1 404 Not Found\r\n" + "Content-Type: text/plain\r\n" +
+        "Content-Length: #{message.size}\r\n" + "Connection: close\r\n"
+        socket.print "\r\n"
+        socket.print message
+    end
     socket.close
 }
